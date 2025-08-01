513dc29f9c8499ac73ffdca53e059d71414387dd
ngodup <ngodupbz@gmail.com>
2025-08-01 16:57:40 +0200

    add viper and replace constant with app.env

---
+++ util/conif.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config store all configuraiton of the application
+// The values are read by viper from a config file or environment variable
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app") // app.env
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
